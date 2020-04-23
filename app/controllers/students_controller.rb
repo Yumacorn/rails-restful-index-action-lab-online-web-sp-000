843295550c6a9f0ebdce14a5c11e86388ef1bf69
Ryan Mui <reachryanmui@gmail.com>
2020-04-23 02:32:07 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,3 +4,9 @@ class StudentsController < ApplicationController
         @list = Student.all
     end
 end
+
+# <!-- <h1>Welcome to the Index</h1> -->
+# <!-- <% @list.each do |name| %>
+#   <p><%= name%>
+#
+# <% end %> -->
